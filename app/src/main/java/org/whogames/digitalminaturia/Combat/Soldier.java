a30913c35a73f78c3421738cc0daf911ee6baefe
Rishi Boggavarapu <83251988+TheFriendlyStudent@users.noreply.github.com>
2025-10-25 14:25:03 -0500

    more file parsing changes

--- app/src/main/java/org/whogames/digitalminaturia/Combat/Soldier.java
+++ app/src/main/java/org/whogames/digitalminaturia/Combat/Soldier.java
@@ -5,6 +5,7 @@ public class Soldier {
     private final static int BASE_HEALTH = 3;
     private final int id;
     private final String type;
+    private String kit;
     private final boolean canDrive;
 
     private static final String types[] = {
@@ -12,10 +13,11 @@ public class Soldier {
             "Anti-Tank Specialist", "Medic", "Engineer", "Squad Leader", "Commanding Officer"
     };
 
-    public Soldier(int id, String type, boolean canDrive) {
+    public Soldier(int id, String type, String kit) {
         this.id = id;
         this.type = type;
-        this.canDrive = canDrive;
+        this.kit = kit;
+        this.canDrive = type.equals("Mechanic");
     }
 
 }
